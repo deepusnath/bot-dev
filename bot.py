e7fe04eebba9f6e966c036e2034a0a0a9180202e
Deepu S Nath <deepusnath@gmail.com>
2021-12-16 01:37:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,4 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send('Hello, Awesome buddy.. I am a bot!')
 
-client.run('OTE5MjA0MDUxMjk4Njk3MjE2.YbSZew.yROw7ROjvVZ16xjNiCzJpeCNUfo')        
+client.run('XXXXXXXXXX')        
